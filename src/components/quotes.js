9b899f80ff83489f83d4023340a12ad9f178a847
tsabz <tonia.saba@gmail.com>
2020-12-15 18:10:09 -0500

    quotes on dom

--- src/components/quotes.js
+++ src/components/quotes.js
@@ -9,13 +9,24 @@ function Quotes() {
     return (
         <div className="containerquotes"> 
             <Typist>
-            <h2> First Sentence </h2>
+            <h2>“Staying home means starving to death for millions displaced in Yemen”</h2>-CNN, April 2015
+            <br />
             <Typist.Delay ms={1000} />
                 <br />
-                <h2> This won't be animated until 500ms after the first sentenced is rendered</h2>
+                <h2> “The war in Yemen is getting worse -- and a civilian catastrophe is looming”</h2>-NPR, 2015
+                <br />
             <Typist.Delay ms={1000} />
                 <br />
-                <h2>This wont be typed till after the 3rd</h2>
+                <h2>“U.S. Confirms It Is Supporting Saudi Military Operations In Yemen”</h2>-Reuters, 2020
+                <br />
+            <Typist.Delay ms={1000} />
+                <br />
+                <h2>“Yemen could lose an entire generation of children to hunger, yet the world remains silent”</h2>-MidEast Monitor, 2020
+                <br />
+            <Typist.Delay ms={1000} />
+                <br />
+                <h2>“UN sounds ‘wake-up call’ to save millions of Yemenis from famine”</h2>-Aljazeera, 2020
+                <br />                
             </Typist>
         </div>
         
